047352266861f6f7bee0996c39dec7fde233c197
uzaif <uzaif313@gmail.com>
2018-05-01 22:21:31 +0530

    basic skeleton for toggle between edit and list

--- src/ProductItem.js
+++ src/ProductItem.js
@@ -5,7 +5,11 @@ import React, { Component } from 'react';
 class ProductItem extends Component {
   constructor(props){
     super(props)
+    this.state = {
+      isEdit:false
+    }
     this.onDelete = this.onDelete.bind(this);
+    this.onEdit = this.onEdit.bind(this);
   }  
     
   onDelete(){
@@ -13,18 +17,40 @@ class ProductItem extends Component {
     onDelete(name);
   }
 
+  onEdit(){
+    this.setState({isEdit:true});
+  }
 
   render() {
-    const {name, price} = this.props
     return (
+      <React.Fragment>
+        {
+          this.state.isEdit ?  this.editProduct() : this.plainList()
+        }
+      </React.Fragment>
+    );
+  }
+
+  editProduct(){
+   return(
+    <h1>Product Edit</h1>
+   ) 
+  }
+
+  plainList(){
+    const {name, price} = this.props
+    return(
               <div>
                 <span>{name}</span>
                 { ` | `}
                 <span>{price}</span>
                 { ` | `}
+                <button onClick={this.onEdit}>Edit</button>
+                { ` | `}
                 <button onClick={this.onDelete}>Delete</button>
               </div>
-    );
+
+    )
   }
 }
 
